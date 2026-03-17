5b69f56131cbe9c46f88a1dbc3106beba5eb76bd
RohanAM6606 <rohan02aug@gmail.com>
2026-03-17 14:59:38 +0530

    fix: resolve Home.jsx imports and structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,31 @@
 import Navbar from "../components/common/Navbar";
 import HeroSection from "../components/hero/HeroSection";
 import About from "../components/about/About";
-<<<<<<< HEAD
-import Domains from "../components/Domains/Domains";
 import Events from "../components/Events/Events";
-import Teams from "../components/Teams/Teams";  
-=======
->>>>>>> b73ede13fea3fa89f166a351703a09296ef286d8
-
+import Teams from "../components/Teams/Teams";
 
 const Home = () => {
-return ( <div> <Navbar />
+  return (
+    <div>
+      <Navbar />
 
-```
-  <div data-section="home">
-    <HeroSection />
-  </div>
+      <div data-section="home">
+        <HeroSection />
+      </div>
 
-  <div data-section="about">
-    <About />
-  </div>
+      <div data-section="about">
+        <About />
+      </div>
 
-<<<<<<< HEAD
-  <div data-section="domains">
-    <Domains />
-    
-  </div>
+      <div data-section="events">
+        <Events />
+      </div>
 
-  <div data-section="events">
-    <Events />
-  </div>
-  <div data-section="teams">
-    <Teams />
-  </div>
- 
-  
-  
-
-=======
-
-  
->>>>>>> b73ede13fea3fa89f166a351703a09296ef286d8
-</div>
-
-
-);
+      <div data-section="teams">
+        <Teams />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
